d39d3188ec1f3642d95101af28e881bfc91e93b4
Fiona Lapham <fionadlapham@gmail.com>
2017-10-15 02:42:01 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(josh)
   puts "Hi, my name is #{josh}."
 end
 
-def introduction_with_language(dan, "ember.js")
-  puts "Hi my name is #{dan} and I am learning to program #{"ember.js"}."
+def introduction_with_language(dan, ember.js)
+  puts "Hi my name is #{dan} and I am learning to program #{ember.js}."
 end
